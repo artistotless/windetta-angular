3e55e1a7bd3b6979205de7129ef653553c35082b
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:41:50 +0300

    added profile state into global IAppStore

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,7 +1,9 @@
 import { ILobbiesState } from "./store/lobbies/lobbies.state";
 import { IMatchesState } from "./store/matches/matches.state";
+import { IProfileState } from "./store/profile/profile.state";
 
 export interface IAppStore {
     lobbies: ILobbiesState;
     matches: IMatchesState;
+    profile: IProfileState;
 }
