22e75ede08b376ffe7c3a200a468a52aaac9a9aa
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:41:31 +0300

    added routing for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomePage } from './pages/home/home.page';
 import { OngoingMatchesListPage } from './pages/ongoing-matches-list/ongoing-matches-list.page';
 import { AuthorizeGuard } from './services/route-guard.service';
 import { LobbiesPage } from './pages/lobbies/lobbies.page';
+import { LoginPage } from './pages/login/login.page';
 
 export const routes: Routes = [
     { path: "", component: HomePage, title: "Windetta | PVP Ton Battles" },
     { path: "matches", component: OngoingMatchesListPage, canActivate: [AuthorizeGuard], title: "Windetta | Matches" },
-    { path: "lobbies", component: LobbiesPage, canActivate: [AuthorizeGuard], title: "Windetta | Lobbies" },
+    { path: "lobbies", component: LobbiesPage, title: "Windetta | Lobbies" },
+    { path: "login", component: LoginPage, title: "Windetta | Login" },
     { path: "**", component: NotFoundPage, title: "404 - NotFound" }
 ];
