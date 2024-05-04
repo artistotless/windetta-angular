1cb46e5611bd69c8eaa27d177e94ac2e870a4f49
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:42:32 +0300

    refactored IProfile model

--- src/app/models/profile.model.ts
+++ src/app/models/profile.model.ts
@@ -1,16 +1,7 @@
 
-export class Profile {
+export interface IProfile {
     id: string;
     displayName: string;
     username: string;
     email: string;
-    isAutheticated!: boolean;
-
-    constructor() {
-        this.id = "a3b122ab-069e-4c02-9b5e-a8e7f150514d";
-        this.displayName = "Loading...";
-        this.email = "Loading...";
-        this.username = "Loading...";
-        this.isAutheticated = false;
-    }
 }
