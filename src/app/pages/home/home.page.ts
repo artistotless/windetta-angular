26008acf7ddec4ca88777a2415ef39ea94e35d9a
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:43:06 +0300

    refactored home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ProfileItemComponent } from '../../ui/profile-item/profile-item.component';
-import { IdentityService } from '../../services/identity.service';
+import { IAppStore } from '../../app.store';
+import { select, Store } from '@ngrx/store';
+import { profile } from '../../store/profile/profile.selectors';
+import { Observable, Subscription } from 'rxjs';
+import { IProfile } from '../../models/profile.model';
+import * as Actions from '../../store/profile/profile.actions';
+import * as Selectors from '../../store/profile/profile.selectors';
 
 @Component({
   selector: 'app-home',
@@ -14,8 +20,9 @@ import { IdentityService } from '../../services/identity.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePage {
+  profile$: Observable<IProfile>;
 
-  constructor(public identity: IdentityService) {
+  constructor(private _store: Store<IAppStore>) {
+    this.profile$ = this._store.pipe(select(profile));
   }
-
 }
