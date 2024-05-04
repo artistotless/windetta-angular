4bc3d77ff39bf376bde02246c2ccf0d98cfe12e3
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:44:03 +0300

    deleted useless code

--- src/app/services/identity.service.ts
+++ src/app/services/identity.service.ts
@@ -1,38 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Profile } from '../models/profile.model';
-import { AsyncSubject, Observable, Subject, tap } from 'rxjs';
+import { IProfile } from '../models/profile.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IdentityService {
 
-  private _profile$: AsyncSubject<Profile> = new AsyncSubject();
-  private _initialized: boolean = false;
-
-  user$: Observable<Profile> = this._profile$.asObservable();
-
   constructor(private _client: HttpClient) {
   }
 
-  authenticate(): Observable<Profile> {
-    if (!this._initialized)
-      return this._client.get<Profile>(`${environment.mvcUrl}/profile`, {
-        withCredentials: true
-      })
-        .pipe(tap(value => {
-
-          this._profile$.next(value);
-          this._profile$.complete();
-          this._initialized = true;
-        }));
-    else
-      return this.user$;
-  }
-
-  setProfile(newProfile: Profile) {
-    this._profile$.next(newProfile);
+  authenticate(): Observable<IProfile> {
+    return this._client.get<IProfile>(`${environment.mvcUrl}/profile`, {
+      withCredentials: true
+    });
   }
 }
