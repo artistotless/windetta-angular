ee9b344ebe1d16973681d565771171d517c45bec
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:38:22 +0300

    [JXoeJq8l] added addition property to state

--- src/app/store/lobbies/lobbies.state.ts
+++ src/app/store/lobbies/lobbies.state.ts
@@ -1,7 +1,9 @@
 import { EntityState } from "@ngrx/entity";
 import { Lobby } from "../../models/lobby.model";
+import { UserLobbyMapEntry } from "../../models/user-lobby-map-entry";
 
 export interface ILobbiesState extends EntityState<Lobby> {
+    currentLobby: UserLobbyMapEntry | undefined | null;
     isLoading: boolean;
     isCached: boolean;
     error: string | null;
