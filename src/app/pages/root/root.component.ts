ae5ea8e8c0cdf7d8c7803e820b0cc7ef6dd7a979
artistotless <neuralink7232050@gmail.com>
2024-05-04 08:43:36 +0300

    refactored root component

--- src/app/pages/root/root.component.ts
+++ src/app/pages/root/root.component.ts
@@ -3,9 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { RouterLink } from '@angular/router';
-import { IdentityService } from '../../services/identity.service';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Subscription } from 'rxjs';
+import { IAppStore } from '../../app.store';
+import { Store } from '@ngrx/store';
+import * as Actions from '../../store/profile/profile.actions'
+
 
 @Component({
   selector: 'app-root',
@@ -19,17 +22,17 @@ export class AppComponent implements OnInit, OnDestroy {
 
   title = "Windetta";
 
-  private authSubscription!: Subscription;
+  private _authSubscription!: Subscription;
 
-  constructor(lib: FaIconLibrary, private _identity: IdentityService) {
-    lib.addIcons(faCoffee);
+  constructor(private _lib: FaIconLibrary, private _store: Store<IAppStore>) {
+    this._lib.addIcons(faCoffee);
   }
 
   ngOnInit(): void {
-    this.authSubscription = this._identity.authenticate().subscribe();
+    this._store.dispatch(Actions.get());
   }
 
   ngOnDestroy(): void {
-    this.authSubscription.unsubscribe();
+    this._authSubscription.unsubscribe();
   }
 }
