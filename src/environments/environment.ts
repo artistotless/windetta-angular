d9cc62c287237e3b6ed0b89b35c22118480533bc
artistotless <neuralink7232050@gmail.com>
2024-05-26 18:19:51 +0300

    updated prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ import { Lobby, LobbyState } from "../app/models/lobby.model";
 
 export const environment = {
     production: true,
-    apiUrl: "https://localhost:7084/api",
-    mvcUrl: "https://localhost:7084",
-    mainHubUrl: "https://localhost:55235/mainHub",
+    apiUrl: "https://spa-proxy.feed78.com/api",
+    mvcUrl: "https://spa-proxy.feed78.com",
+    mainHubUrl: "https://main.feed78.com/mainHub",
     enableHttpToasts: false,
     testLobbies: [
         {
