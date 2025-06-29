8c08d79489d6afd838b15323c8f951676f0cd6b8
artistotless <neuralink7232050@gmail.com>
2025-06-29 15:19:34 +0300

    task#G6xkAarw

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: false,
-    apiUrl: "https://localhost:55004/api",
-    mvcUrl: "https://localhost:55004",
-    mainHubUrl: "https://localhost:55001/mainHub",
+    // В режиме разработки используем относительные пути - SPA proxy перенаправит на backend
+    apiUrl: "/api",
+    mvcUrl: "",
+    mainHubUrl: "https://localhost:55001/mainHub", // SignalR Hub остается прямым подключением
     enableHttpToasts : false
 };
